1bf6ecaaee0e419799755bd29e316547027f531a
Alexander Schelchere <ascellie@gmail.com>
2019-08-25 03:06:46 -0400

    spec(calculator): add 3 more tests

--- ruby/calculator/spec/calculator_spec.rb
+++ ruby/calculator/spec/calculator_spec.rb
@@ -3,6 +3,7 @@ require_relative "../lib/calculator.rb"
 
 
 describe Calculator do 
+
   describe "#add" do 
     it "returns the sum of its two arguments" do
       calc = Calculator.new
@@ -10,4 +11,29 @@ describe Calculator do
       expect(calc.add(7, 3)).to eq(10)
     end
   end
+
+  describe "#subtract" do 
+    it "returns the difference of its two arguments" do
+      calc = Calculator.new
+
+      expect(calc.subtract(13, 4)).to eq(9)
+    end
+  end
+
+  describe "#multiply" do 
+    it "returns the product of its two arguments" do
+      calc = Calculator.new
+
+      expect(calc.multiply(25, 8)).to eq(200)
+    end
+  end
+
+  describe "#divide" do 
+    it "returns the quotient of its two arguments" do
+      calc = Calculator.new
+
+      expect(calc.divide(15, 3)).to eq(5)
+    end
+  end
+
 end
