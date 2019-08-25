a004410ce235b53f76c994356fe53d9eec83be6e
Alexander Schelchere <ascellie@gmail.com>
2019-08-25 11:00:34 -0400

    Person: starter code

---
+++ ruby/person/spec/person_spec.rb
@@ -0,0 +1,14 @@
+require "rspec/autorun"
+require_relative "../lib/person.rb"
+
+describe Person do
+  describe "#full_name" do
+    it "concatenates first name, middle name, and last name with spaces"
+    it "does not add extra spaces if middle name is missing"
+  end
+  
+  describe "#full_name_with_middle_initial"
+
+  describe "#initials"
+
+end
