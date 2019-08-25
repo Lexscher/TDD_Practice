5e42e66546f0ecd13a5703caa581db7552f6a4d5
Alexander Schelchere <ascellie@gmail.com>
2019-08-25 11:09:30 -0400

    spec(person): first test written

--- ruby/person/spec/person_spec.rb
+++ ruby/person/spec/person_spec.rb
@@ -3,12 +3,25 @@ require_relative "../lib/person.rb"
 
 describe Person do
   describe "#full_name" do
-    it "concatenates first name, middle name, and last name with spaces"
-    it "does not add extra spaces if middle name is missing"
+    it "concatenates first name, middle name, and last name with spaces" do
+      bart = Person.new(first_name:  "Bartholomew", middle_name: "JoJo", last_name: "Simpson")
+      
+      expect(bart.full_name).to eq("Bartholomew JoJo Simpson")
+    end
+
+    it "does not add extra spaces if middle name is missing" do
+      james = Person.new(first_name:  "james", last_name: "bond")
+
+      expect(james.full_name).to eq("james bond")
+    end
   end
   
-  describe "#full_name_with_middle_initial"
+    # describe "#full_name_with_middle_initial" do
 
-  describe "#initials"
+    # end
+
+    # describe "#initials" do
+
+    # end
 
 end
